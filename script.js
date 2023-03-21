c12110dd8a6d41205dee6da31f920284e2506955
Sebastian Sala <sebastian_orko@hotmail.com>
2023-03-21 14:01:07 -0600

    functional to point 5

--- script.js
+++ script.js
@@ -5,12 +5,21 @@ const read = document.getElementById("read");
 const add = document.querySelector("#add");
 const main = document.querySelector(".main-flex");
 
-let myLibrary = [];
+const myLibrary = [];
+let idCounter = 0;
 
 function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.id = ++idCounter;
+}
+
+function removeBook(book, card){
+  const index = myLibrary.findIndex(b => b.id === book.id);
+    myLibrary.splice(index, 1);
+    main.removeChild(card);
+    console.log(myLibrary)
 }
 
 function displayBook(book) {
@@ -31,34 +40,44 @@ function displayBook(book) {
 
   const read = document.createElement("button");
   read.textContent = `In progress`;
-  read.setAttribute('id', 'button');
-  read.classList.add('progress');
-  card.appendChild(read); 
+  read.setAttribute("id", `read-${book.id}`);
+  read.classList.add("progress");
+  card.appendChild(read);
+
+  const remove = document.createElement("button");
+  remove.textContent = `Remove`;
+  remove.setAttribute("id", `remove-${book.id}`);
+  remove.classList.add("remove");
+  card.appendChild(remove);
 
   main.appendChild(card);
 
-  read.addEventListener('click', (event) => {
-    if(event.target.textContent === 'In progress'){
-      event.target.textContent = 'Completed';
-      event.target.classList.remove('progress');
-      event.target.classList.add('completed');
-    }else{
-      event.target.textContent = 'In progress';
-      event.target.classList.remove('completed');
-      event.target.classList.add('progress');
+  read.addEventListener("click", (event) => {
+    if (event.target.textContent === "In progress") {
+      event.target.textContent = "Completed";
+      event.target.classList.remove("progress");
+      event.target.classList.add("completed");
+    } else {
+      event.target.textContent = "In progress";
+      event.target.classList.remove("completed");
+      event.target.classList.add("progress");
     }
-  })
+  });
+
+  remove.addEventListener('click', () => removeBook(book, card));
 }
 
-function addBookToLibrary() {
-  add.addEventListener("click", (event) => {
+
+function addBookToLibrary(event) {
     event.preventDefault();
     const book = new Book(title.value, author.value, pages.value);
-    myLibrary.push(book)
-    console.log(myLibrary)
+    myLibrary.push(book);
     displayBook(book);
-  });
+    console.log(myLibrary)
 }
 
-addBookToLibrary();
+add.addEventListener("click", (event) => addBookToLibrary(event));
+
+
+
 
